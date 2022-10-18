6d03f26f8b7e06c85e6fcfd8305e4572d66e5f32
Perceval <mtrper001@myuct.ac.za>
2022-10-18 13:11:22 +0200

    one to many outstanding on the VariantStudy model

--- agmp/models.py
+++ agmp/models.py
@@ -12,21 +12,43 @@ class Drugagmp(models.Model):
     state = models.CharField(max_length=50, null=True, blank=True)
 
 class Geneagmp(models.Model):
-    gen_id = models.CharField(max_length=50, null=True, blank=True)
-    source_db2 = models.CharField(max_length=50, null=True, blank=True)
+    # gen_id = models.CharField(max_length=50, null=True, blank=True)
+    # source_db = models.CharField(max_length=50, null=True, blank=True)
     chromosome = models.CharField(max_length=50, null=True, blank=True)
     function = models.CharField(max_length=50, null=True, blank=True)
     gene_name= models.CharField(max_length=50, null=True, blank=True)
-    uniprot_ac= models.CharField(max_length=50, null=True, blank=True)
+    uniprot= models.CharField(max_length=50, null=True, blank=True)
 
+class Studyagmp(models.Model):
+    data_ac = models.CharField(max_length=50, null=True, blank=True)
+    publication_id = models.CharField(max_length=50, null=True, blank=True)
+    publication_type = models.CharField(max_length=50, null=True, blank=True)
+    publication_year = models.CharField(max_length=50, null=True, blank=True)
+    study_type = models.CharField(max_length=50, null=True, blank=True)
+    title = models.CharField(max_length=50, null=True, blank=True)
 
+class Phenotypeagmp(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+   
 
 class Variantagmp(models.Model):
     allele = models.CharField(max_length=50, null=True, blank=True)
     drugagmp = models.ForeignKey(Drugagmp, on_delete=models.CASCADE, default="DRUG0",null=True, blank=True)
     source_db = models.CharField(max_length=50, null=True, blank=True)
     geneagmp = models.ForeignKey(Geneagmp, on_delete=models.CASCADE, default="GENE0",null=True, blank=True)
+    studyagmp = models.ForeignKey(Studyagmp, on_delete=models.CASCADE,null=True, blank=True)
+    phenotypeagmp = models.ForeignKey(Phenotypeagmp, on_delete=models.CASCADE,null=True, blank=True)
 
+
+class VariantStudyagmp(models.Model):
+    variantagmp = models.ForeignKey(Variantagmp, on_delete=models.CASCADE,null=True, blank=True)
+    country_participant = models.CharField(max_length=50, null=True, blank=True)
+    ethnicity = models.CharField(max_length=50, null=True, blank=True)
+    geographical_regions = models.CharField(max_length=50, null=True, blank=True)
+    notes = models.CharField(max_length=50, null=True, blank=True)
+    p_value = models.CharField(max_length=50, null=True, blank=True)
+  
+  
 # class Geneagmp(models.Model):
 #     gen_id = models.CharField(max_length=50, null=True, blank=True)
 
--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,10 +1,13 @@
 import csv
 
-from agmp.models import Variantagmp, Drugagmp, Geneagmp
+from agmp.models import Variantagmp, Drugagmp, Geneagmp, Studyagmp, Phenotypeagmp, VariantStudyagmp
 # python3 manage.py runscript load_data
 # g is for Gene
 # d is for Drug
 # v is for Variant
+# s is for Study
+# p is for Phenotype
+
 
 def run():
 
@@ -15,22 +18,29 @@ def run():
     Variantagmp.objects.all().delete()
     Drugagmp.objects.all().delete()
     Geneagmp.objects.all().delete()
+    Studyagmp.objects.all().delete()
+    Phenotypeagmp.objects.all().delete()
+    # VariantStudyagmp.objects.all().delete()
     
 
 
     for row in reader:
         # print(row)
-
+        p, created = Phenotypeagmp.objects.get_or_create()
+        s, created = Studyagmp.objects.get_or_create(data_ac=row[17],publication_id=row[1],publication_type=[18],publication_year=row[21],study_type=row[11],title=row[19])
         d, created = Drugagmp.objects.get_or_create(drug_bank_id=row[2], drug_name=row[3], indication=row[4], iupac_name_seq=row[5], state=row[6])
-        g, created = Geneagmp.objects.get_or_create(gen_id=row[1])
-        v = Variantagmp(allele=row[0], drugagmp=d, geneagmp=g,source_db=row[2])
+        g, created = Geneagmp.objects.get_or_create(chromosome=row[14], function=row[16], uniprot=row[15],gene_name=row[9])
+        v = Variantagmp(studyagmp=s,drugagmp=d,phenotypeagmp=p, geneagmp=g,allele=row[0], source_db=row[2])
         v.save()
 
         no_of_genes = Geneagmp.objects.all().count()
         no_of_drugs = Drugagmp.objects.all().count()
         no_of_variants = Variantagmp.objects.all().count()
+        no_of_studies = Studyagmp.objects.all().count()
+        no_of_phenotypes = Phenotypeagmp.objects.all().count()
 
     print(" ############ IMPORT JOB ENDED ############ \n")
+    print(f"{no_of_studies}: Genes IMPORTED")
     print(f"{no_of_genes}: Genes IMPORTED") 
     print(f"{no_of_drugs}: DRUGS IMPORTED") 
     print(f"{no_of_variants}: VARIANTS IMPORTED \n")
